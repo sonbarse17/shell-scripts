a89f2692e61875ec5d560bb8041a3aa91c15e605
Sushant Sonbarse <sushantsonbarse90@gmail.com>
2025-03-24 14:13:54 +0530

    Update trivy.sh

--- trivy.sh
+++ trivy.sh
@@ -1,9 +1,1 @@
-$ sudo vim /etc/yum.repos.d/trivy.repo
-[trivy]
-name=Trivy repository
-baseurl=https://aquasecurity.github.io/trivy-repo/rpm/releases/$releasever/$basearch/
-gpgcheck=0
-enabled=1
-$ sudo yum -y update
-$ sudo yum -y install trivy
-$ trivy -v
+rpm -ivh https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.rpm
