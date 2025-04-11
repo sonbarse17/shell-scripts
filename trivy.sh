d2c3be9ff30f9b952ee20976ae9bc727464290aa
Sushant Sonbarse <sushantsonbarse90@gmail.com>
2025-04-11 12:26:29 +0530

    Update trivy.sh

--- trivy.sh
+++ trivy.sh
@@ -1,1 +1,1 @@
-rpm -ivh https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.rpm
+sudo rpm -ivh https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.rpm
